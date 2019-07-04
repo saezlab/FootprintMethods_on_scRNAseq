dc9721dd7c84a0d4608fee2dab857482a5fa4920
Christian <cholland2408@gmail.com>
2019-07-04 11:36:16 +0200

    Added my ggplot theme

--- src/my_ggplot_themes.R
+++ src/my_ggplot_themes.R
@@ -1,6 +1,8 @@
-library(ggplot2)
-library(cowplot)
-
 my_theme = function() {
-  background_grid(major = "xy", minor = "none", size.major = 0.4)
+  background_grid(major = "xy", minor = "none", size.major = 0.4) +
+    theme(title = element_text(size=16),
+          axis.text = element_text(size=14),
+          legend.text = element_text(size=14),
+          legend.title = element_blank()
+    )
 }
