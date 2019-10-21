4e985567db6a57e5aedf196c61f03da997efd597
Christian <cholland2408@gmail.com>
2019-10-21 15:40:25 +0200

    Updated my ggplot theme

--- src/my_ggplot_themes.R
+++ src/my_ggplot_themes.R
@@ -1,9 +1,18 @@
-my_theme = function() {
-  background_grid(major = "xy", minor = "none", size.major = 0.4) +
+my_theme = function(grid = NULL) {
+  
+  if (is.null(grid)) {
+    p = background_grid(major = "xy", minor = "none", size.major = 0.4) 
+  } else if (grid == "y") {
+    p = background_grid(major = "y", minor = "none", size.major = 0.4) 
+  } else if (grid == "x") {
+    p = background_grid(major = "x", minor = "none", size.major = 0.4) 
+  } else if (grid == "no") {
+    p = theme()
+  }
+  p = p + 
     theme(title = element_text(size=14),
           axis.text = element_text(size=14),
           legend.text = element_text(size=14),
-          legend.title = element_blank(),
           strip.background = element_rect(colour = "white", fill="white"),
           strip.text.x  = element_text(size=14)
     )
